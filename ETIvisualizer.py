fc9e80caa1bab88bfd08c5c0fc85e53e3c5effc6
EuReinoso <lucas.reinoso.br@gmail.com>
2022-02-11 19:52:03 -0300

    Logo and v0.3

--- ETIvisualizer.py
+++ ETIvisualizer.py
@@ -74,29 +74,32 @@ LIN_FONTE_2 = {
 }
 
 
+con = None
 eti_cod = None
 last_eti = 0
 
+def connect_database(file_path):
+    # Conecta ao Banco de Dados MS Access 
+    # (Necessário instalar os Drivers do MS Access)
 
-#INICIO
-os.system('cls')
-print('IMPLANTAÇÃO - VISUALIZADOR DE ETIQUETA\n')
-
-while True: 
-    
-    file_path = input('\nDigite o diretório do arquivo ETI.mdb: ')
-
+    global con
     try:
         con = pyodbc.connect(r'Driver={};DBQ={};'.format(DRIVER, file_path))
         print('Database Conectado - OK!')
-        break
+        
+        return True
     except pyodbc.Error as e:
         print("Erro ao conectar ao Database: ", e)
         print()
         print("- VERIFIQUE SE O DIRETORIO ESTÁ CORRETO")
         print("- VERIFIQUE SE OS DRIVERS DO MS ACCESS ESTÃO INSTALADOS")
+        
+        return False
 
-while True:
+def write_csv():
+    # Lê o arquivo ETI.mdb (MS Access) e transforma em .CSV
+
+    global con
     try:
         cur = con.cursor()
         table_etiqueta = cur.execute(SELECT_ETIQUETA).fetchall()
@@ -114,17 +117,23 @@ while True:
 
     except:
         print('Erro ao CRIAR Tabelas CSV.')
-        exit()
 
+def read_csv():
+    # Lê os arquivos .CSV criados e transforma em DataFrames 
+    # (Transformei em DataFrame pois são mais fáceis e rápidos de manipular)
 
     try:
         df_etiqueta = pd.read_csv('ETIQUETA.csv', names=ETIQUETA_COL_NAMES)
         df_linha = pd.read_csv('LINHA.csv', names=LINHA_COL_NAMES)
         print('Leitura do CSV - OK!')
-
+        return df_etiqueta, df_linha
     except:
         print('Erro ao LER tabelas CSV.')
 
+def select_eti(df_etiqueta):
+    # Lê o DataFrame e mostra os Nomes das ETI's ao usuário
+
+    global eti_cod
 
     while eti_cod == None: 
 
@@ -146,8 +155,12 @@ while True:
                 os.system('cls')
                 print('\nERRO - Etiqueta NÃO encontrada!')
                 print('- VERIFIQUE SE O CAMPO LIN_CODIGO E LIN_NOME DO ARQUIVO MDB ESTÃO TROCADOS')
-    
-    #LEITURA DE PARAMETROS DA ETI
+
+def eti_generate(df_etiqueta, df_linha, orien):
+    # Lê os DataFrames e transforma em uma imagem
+
+    global last_eti
+
     eti_name     = df_etiqueta.loc[df_etiqueta['ETI_CODIGO'] == eti_cod]['ETI_NOME'].values[0]
     eti_altura   = df_etiqueta.loc[df_etiqueta['ETI_CODIGO'] == eti_cod]['ETI_ALTURA'].values[0]
     eti_largura  = df_etiqueta.loc[df_etiqueta['ETI_CODIGO'] == eti_cod]['ETI_LARGURA'].values[0]
@@ -156,86 +169,125 @@ while True:
 
     eti_linhas = df_linha.loc[df_linha['LIN_ETIQUETA'] == eti_cod]
 
-    orien = 1
+    dpi = 150
+    fig, ax = plt.subplots(figsize=(eti_largura, eti_altura), dpi=dpi)
 
-    #LOOP PRINCIPAL
-    while True:
-        print('\nMENU')
-        print('1 - Ver Etiqueta')
-        print('2 - Atualizar Etiqueta')
-        print('3 - Mudar Orientação')
-        print('4 - Sair')
-        i = input("Escolha a opção: ")
-
-        if i == '1':
-            fig, ax = plt.subplots(figsize=(eti_largura, eti_altura), dpi=150)
-
-            ax.set_facecolor('#cccccc')
+    ax.set_facecolor('#cccccc')
 
 
-            plt.xlim(0 - eti_marg_esq, eti_largura + eti_marg_cen)
+    plt.xlim(0 - eti_marg_esq, eti_largura + eti_marg_cen)
 
-            if orien == 1:
-                plt.ylim(0, eti_altura)
-            elif orien == 0:
-                plt.ylim(eti_altura, 0)
+    if orien == 1:
+        plt.ylim(0, eti_altura)
+    elif orien == 0:
+        plt.ylim(eti_altura, 0)
 
 
-            rect = mpl.patches.Rectangle((0, 0), eti_largura, eti_altura, color='white')
+    rect = mpl.patches.Rectangle((0, 0), eti_largura, eti_altura, color='white')
 
-            df = eti_linhas
+    df = eti_linhas
 
-            #COLUNAS
-            rows = len(df.index)
-            for i in range(rows):
-                if df.iloc[i]['LIN_TIPO'] == 1:
-                    col = df.iloc[i]['LIN_COLUNA']
-                    size = df.iloc[i]['LIN_FONTE']
-                    x = df.iloc[i]['LIN_MARGEN']
-                    y = df.iloc[i]['LIN_ALTURA']
-                    font_size = LIN_FONTE_1[str(size)]
-                    marg = 0
-                    if pd.notnull(df.iloc[i]['LIN_TEXTO']):
-                        text = df.iloc[i]['LIN_TEXTO'].replace(' ', '   ')
-                        marg = len(text) / 8
-                        ax.text(x, y, '{}'.format(text),size=font_size)
+    #COLUNAS
+    rows = len(df.index)
+    for i in range(rows):
+        if df.iloc[i]['LIN_TIPO'] == 1:
+            col = df.iloc[i]['LIN_COLUNA']
+            size = df.iloc[i]['LIN_FONTE']
+            x = df.iloc[i]['LIN_MARGEN']
+            y = df.iloc[i]['LIN_ALTURA']
+            font_size = LIN_FONTE_1[str(size)]
+            marg = 0
+            if pd.notnull(df.iloc[i]['LIN_TEXTO']):
+                text = df.iloc[i]['LIN_TEXTO'].replace(' ', '   ')
+                marg = len(text) / 8
+                ax.text(x, y, '{}'.format(text),size=font_size)
 
-                    text = LIN_COLUNA_1[str(col)]
-                    ax.text(x + marg, y, '{}'.format(text),size=font_size)
+            text = LIN_COLUNA_1[str(col)]
+            ax.text(x + marg, y, '{}'.format(text),size=font_size)
 
-                
-                if df.iloc[i]['LIN_TIPO'] == 2:
-                    x = df.iloc[i]['LIN_MARGEN']
-                    y = df.iloc[i]['LIN_ALTURA']
-                    width = x + LIN_FONTE_2[str(df.iloc[i]['LIN_FONTE'])][0]
-                    height = y + LIN_FONTE_2[str(df.iloc[i]['LIN_FONTE'])][1]
-                    
-                    barcode_img = plt.imread('barcode.png')
-
-                    plt.imshow(barcode_img, extent=[x, width, y, height], aspect='auto', zorder=2)
-
-            ax.add_patch(rect)
-            plt.savefig('IMAGENS/eti_{}_{}.jpg'.format(eti_name, last_eti))
-            last_eti += 1
-            
-            os.system('cls')
-            print("\nIMAGEM BAIXADA!")
-
-        elif i == '2':
-            os.system('cls')
-            print('\nATUALIZANDO ETIQUETA...')
-            break
         
-        elif i == '3':
+        if df.iloc[i]['LIN_TIPO'] == 2:
+            x = df.iloc[i]['LIN_MARGEN']
+            y = df.iloc[i]['LIN_ALTURA']
+            width = x + LIN_FONTE_2[str(df.iloc[i]['LIN_FONTE'])][0]
+            height = y + LIN_FONTE_2[str(df.iloc[i]['LIN_FONTE'])][1]
+            
+            barcode_img = plt.imread('barcode.png')
+
+            plt.imshow(barcode_img, extent=[x, width, y, height], aspect='auto', zorder=2)
+
+        if df.iloc[i]['LIN_TIPO'] == 3:
+            logo_path = df.iloc[i]['LIN_TEXTO']
+            logo_img = plt.imread(logo_path)
+            img_plot = ax.imshow(logo_img, zorder=2, aspect='auto')
+
+            x = df.iloc[i]['LIN_MARGEN']
+            y = df.iloc[i]['LIN_ALTURA']
+
+            #transform scale (pix to cm)
+            img_extent = img_plot.get_extent()
+
+            # cm/px on 150 dpi
+            cm = 0.014
+
             if orien == 1:
-                orien = 0
+                plt.imshow(logo_img, extent=(x, x + img_extent[1] * cm,  y, y + img_extent[2] * cm), aspect='auto', zorder=2)
             else:
-                orien = 1
+                plt.imshow(logo_img, extent=(x, x + img_extent[1] * cm, y, y + img_extent[2] * cm), aspect='auto', zorder=2, origin='lower')
 
-            os.system('cls')
-            print('\nORIENTAÇÃO MUDADA!')
 
-        elif i == '4':
-            exit()
+
+    ax.add_patch(rect)
+    plt.savefig('IMAGENS/eti_{}_{}.jpg'.format(eti_name, last_eti))
+    last_eti += 1
     
+    os.system('cls')
+    print("\nIMAGEM BAIXADA!")
 
+def main():
+    os.system('cls')
+    print('IMPLANTAÇÃO - VISUALIZADOR DE ETIQUETA\n')
+
+    while True:
+        file_path = input('\nDigite o diretório do arquivo ETI.mdb: ')
+        if connect_database(file_path):
+            break
+
+    while True:
+        orien = 1 #orientação da etiqueta
+
+        write_csv()
+        df_etiqueta, df_linha = read_csv()
+        select_eti(df_etiqueta)
+
+
+        #LOOP MENU
+        while True:
+            print('\nMENU')
+            print('1 - Ver Etiqueta')
+            print('2 - Atualizar Etiqueta')
+            print('3 - Mudar Orientação')
+            print('4 - Sair')
+            i = input("Escolha a opção: ")
+
+            if i == '1':
+                eti_generate(df_etiqueta, df_linha, orien)
+
+            elif i == '2':
+                os.system('cls')
+                print('\nATUALIZANDO ETIQUETA...')
+                break
+            
+            elif i == '3':
+                if orien == 1:
+                    orien = 0
+                else:
+                    orien = 1
+
+                os.system('cls')
+                print('\nORIENTAÇÃO MUDADA!')
+
+            elif i == '4':
+                exit()
+
+main()
